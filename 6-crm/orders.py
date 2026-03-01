79f9c7eee299e816fece2b7500add2058f1a7344
Victor Stepanov <st3panovvi@yandex.ru>
2026-03-01 19:31:24 +0300

    feat: Домашнее задание - Модули

---
+++ 6-crm/orders.py
@@ -0,0 +1,31 @@
+"""
+Docstring для orders
+"""
+
+
+def create_order():
+    """
+    Docstring для create_order
+    """
+    pass
+
+
+def list_orders():
+    """
+    Docstring для list_orders
+    """
+    pass
+
+
+def edit_order():
+    """
+    Docstring для edit_order
+    """
+    pass
+
+
+def remove_order():
+    """
+    Docstring для remove_order
+    """
+    pass
---
+++ 6-crm/utils/validators.py
@@ -0,0 +1,10 @@
+"""
+Docstring для validators
+"""
+
+
+def validate():
+    """
+    Docstring для validate
+    """
+    pass
